e2903107aa23e6eabe26c58f3dfa6f73a3085885
Minimalon <minimalonchik@gmail.com>
2024-11-01 15:07:26 +0300

    init

---
+++ src/gunicorn_conf.py
@@ -0,0 +1,14 @@
+from multiprocessing import cpu_count
+from config import dir_path
+
+bind = "10.8.16.18:8180"
+
+# Worker Options
+workers = cpu_count() + 1
+worker_class = 'uvicorn.workers.UvicornWorker'
+
+# Logging Options
+loglevel = 'debug'
+logs_dir = dir_path / 'logs'
+accesslog = logs_dir / 'access_log.log'
+errorlog = logs_dir / 'errorlog.log'
