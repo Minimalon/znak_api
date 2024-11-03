17fb44cbadc8dbcfb04ed289f47c438a14ace171
Minimalon <minimalonchik@gmail.com>
2024-11-03 11:17:10 +0300

    .

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,6 +1,6 @@
 from base64 import b64encode
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 
 from src.auth.dependencies import depen_auth
 from src.znak import Znak
@@ -15,8 +15,11 @@ router = APIRouter(
 async def auth(
         params=Depends(depen_auth)
 ) -> str:
-    znak = Znak(params['inn'])
-    login_data = await znak.get_uuid_and_data()
-    signing = await znak.cryproPro.sign_data(b64encode(login_data['data'].encode()).decode())
-    sign_data = await znak.simple_signin(login_data['uuid'], signing, inn=params['inn'])
-    return sign_data['token']
+    try:
+        znak = Znak(params['inn'])
+        login_data = await znak.get_uuid_and_data()
+        signing = await znak.cryproPro.sign_data(b64encode(login_data['data'].encode()).decode())
+        sign_data = await znak.simple_signin(login_data['uuid'], signing, inn=params['inn'])
+        return sign_data['token']
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
